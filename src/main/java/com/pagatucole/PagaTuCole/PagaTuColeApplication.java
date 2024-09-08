b62f7c2571513e99da92e030943d647d9a5ecc70
Emmanuel Ochoa Carreño <gudalvxd@gmail.com>
2024-09-07 20:16:17 -0500

    Update PagaTuColeApplication.java

--- src/main/java/com/pagatucole/PagaTuCole/PagaTuColeApplication.java
+++ src/main/java/com/pagatucole/PagaTuCole/PagaTuColeApplication.java
@@ -9,5 +9,17 @@ public class PagaTuColeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PagaTuColeApplication.class, args);
 	}
-
+@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
